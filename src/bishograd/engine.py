3f98d0b185c8ffb674541128e93c1845e88d0368
AK3847 <abdulkadir.co21d2@scet.ac.in>
2024-07-17 23:43:07 +0530

    backward_propagation-1

--- src/bishograd/engine.py
+++ src/bishograd/engine.py
@@ -1,3 +1,4 @@
+import math
 from typing import Self
 
 
@@ -6,6 +7,8 @@ class Hako:
         self.data = data
         self._prev = set(_children)
         self._op = _op
+        self.grad = 0
+        self._backward = lambda: None
 
     def __repr__(self) -> str:
         return f"Hako(data : {self.data})"
@@ -13,9 +16,51 @@ class Hako:
     def __add__(self, other) -> Self:
         other = other if isinstance(other, Hako) else Hako(other)
         out = Hako(self.data + other.data, (self, other), "+")
+
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad
+
+        out._backward = _backward
         return out
 
     def __mul__(self, other) -> Self:
         other = other if isinstance(other, Hako) else Hako(other)
         out = Hako(self.data * other.data, (self, other), "*")
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
         return out
+
+    def __rmul__(self, other):
+        return self * other
+
+    def tanh(self):
+        x = self.data
+        tanh_value = (math.exp(2 * x) - 1) / (math.exp(2 * x) + 1)
+        out = Hako(tanh_value, (self,), "tanh")
+
+        def _backward():
+            self.grad += (1 - tanh_value**2) * out.grad
+
+        out._backward = _backward
+        return out
+
+    def backward(self):
+        topo = []
+        vis = set()
+
+        def build_topo(curr):
+            if curr not in vis:
+                vis.add(curr)
+                for child in curr._prev:
+                    build_topo(child)
+                topo.append(curr)
+
+        build_topo(self)
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
